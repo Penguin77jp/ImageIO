ea8a832201f92cf5b7b205354f462200eda90d06
Penguin_Desktop <koutei7penguin.jp@gmail.com>
2019-10-03 20:04:45 +0900

    PixInfoを配列からvectorを引数にした

---
+++ bmp.cpp
@@ -0,0 +1,43 @@
+#include "bmp.h"
+
+bmp::bmp(const char* fileName, const int width, const int height, std::vector<PixelInfo> &getPix) : file(std::ofstream(fileName, std::ios::binary | std::ios::out)), width(width), height(height) {
+
+  const int bfOffBits = 54;
+  const int biBitCount = 24;
+  const int biCompression = 0;
+  const int PaddingSize = width * 3 % 4 == 0 ? 0 : 4 - width * 3 % 4;
+  const int biSizeImage = 3 * width * height;
+  const int fileSize = 14 + 44 + biSizeImage;
+  const int biXPixPerMeter = 0;
+  const int biYPixPerMeter = 0;
+  const int biClrUsed = 0;
+  const int biCirImportant = 0;
+
+  BinaryWrite('MB', 2);
+  BinaryWrite(fileSize, 4);
+  BinaryWrite(0, 2);
+  BinaryWrite(0, 2);
+  BinaryWrite(bfOffBits, 4);
+  BinaryWrite(40, 4);
+  BinaryWrite(width, 4);
+  BinaryWrite(height, 4);
+  BinaryWrite(1, 2);
+  BinaryWrite(biBitCount, 2);
+  BinaryWrite(biCompression, 4);
+  BinaryWrite(biSizeImage, 4);
+  BinaryWrite(biXPixPerMeter, 4);
+  BinaryWrite(biYPixPerMeter, 4);
+  BinaryWrite(biClrUsed, 4);
+  BinaryWrite(biCirImportant, 4);
+
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      BinaryWrite(getPix.at(y * width + x).b, 1);
+      BinaryWrite(getPix.at(y * width + x).g, 1);
+      BinaryWrite(getPix.at(y * width + x).r, 1);
+    }
+    for (int x = 0; x < PaddingSize; x++) {
+      BinaryWrite(0, 1);
+    }
+  }
+}
